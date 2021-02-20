f2b8867a8a985a86a033b925171504428e27d0a3
DongYongfei36 <54470509+DongYongfei36@users.noreply.github.com>
2021-02-20 21:55:58 +0800

    test01

--- machine_learing/vector_ops.cpp
+++ machine_learing/vector_ops.cpp
@@ -12,7 +12,7 @@ namespace machine_learning{
     // 重载<< 打印二维数组
     template <typename T>
     std::ostream &operator<<(std::ostream &out, std::vector<std::valarray<T>> const &A){
-        out.precision(4);
+        out.precision(4);   // 取数据的前四位有效数字
         for(const auto &a : A){
             for(const auto &x : a){
                 std::cout << x << ' ';
@@ -43,6 +43,17 @@ namespace machine_learning{
         return out;
     }
 
+    template <typename T>
+    std::ostream &operator<<(std::ostream &out, std::valarray<T> const &A){
+        out.precision(4);
+        for(const auto &a : A){
+            std::cout << a << ' ';
+        }
+        std::cout << std::endl;
+        return out;
+    }
+
+
 
     // 将element插入1D vector
     template <typename T>
@@ -64,7 +75,19 @@ namespace machine_learning{
         weekdays.push_back(2);
         weekdays.push_back(3);
         weekdays.push_back(4);
-        std::cout << "print 1D: " << weekdays;
+        weekdays.push_back(5);
+        std::cout << "print 1D vector: " << weekdays;
+
+        //--------------------------------
+        // 创建一个大小为4、值全为3的valarray
+        std::valarray<int> valarray_A(3, 4);
+        int a = 5;
+        std::valarray<int> B = insert_element(valarray_A, a);
+        std::cout << "insert_element: " << B << std::endl;
+
+        // ----------------------------------
+        std::pair<int, int> pair_A = {1, 2};
+        std::cout << "print pair = "<< pair_A << std::endl;
 
     }
 }
