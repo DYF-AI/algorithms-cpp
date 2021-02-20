e598ceba37881fde536c244449c37fb7a049a7de
DYF-AI <54470509+DYF-AI@users.noreply.github.com>
2021-02-20 23:28:30 +0800

    DenseLayer

--- machine_learing/neural_network.cpp
+++ machine_learing/neural_network.cpp
@@ -13,6 +13,7 @@ namespace machine_learning{
 
 namespace neural_network{
 
+// activations
 namespace activations{
 
     double sigmoid(const double &x) { return 1.0 / (1.0 + std::exp(-x)); }
@@ -22,6 +23,7 @@ namespace activations{
      * @param X Value
      * @return Returns derivative of sigmoid(x)
     */
+    // 导数
     double dsigmoid(const double &x) { return x * (1 - x); }
 
     double relu(const double &x) { return std::max(0.0, x); }
@@ -39,15 +41,36 @@ namespace util_function{
 }
 
 namespace layers{
-class DenseLayes{
+// neural_network::layers::DenseLayers
+class DenseLayer{
 public:
-    double (*activation_function)(const double &);
-    double (*dactivation_function)(const double &);
+    double (*activation_function)(const double &);   // 激活函数
+    double (*dactivation_function)(const double &);  // 激活函数的导数
     int neurons;            // 神经元数量
     std::string activation; // 激活函数
-    std::vector<std::valarray<double>> kernel; // 权重
+    std::vector<std::valarray<double>> kernel; // 权重 二维数组
 
     // 构造函数
+    DenseLayer(const int &neurons, const std::string &activation,
+               const std::pair<size_t, size_t> &kernel_shape, 
+               const bool &rand_kernel) {
+        if (activation == "sigmoid"){
+            activation_function = neural_network::activations::sigmoid;
+            dactivation_function = neural_network::activations::dsigmoid;
+        } else if (activation == "relu") {
+            activation_function = neural_network::activations::relu;
+            dactivation_function = neural_network::activations::drelu;
+        } else if (activation == "tanh") {
+            activation_function = neural_network::activations::tanh;
+            dactivation_function = neural_network::activations::dtanh;
+        } else if (activation == "none") {
+            activation_function = neural_network::util_function::identity_function;
+            dactivation_function = neural_network::util_function::identity_function;
+        }
+        
+                  
+
+    }
     
 };
 }
