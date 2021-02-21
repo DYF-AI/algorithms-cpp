9591c18d28415c9d785e14d1f0f682962df5158f
DYF-AI <54470509+DYF-AI@users.noreply.github.com>
2021-02-21 11:47:46 +0800

    Update vector_ops.cpp

--- machine_learing/vector_ops.cpp
+++ machine_learing/vector_ops.cpp
@@ -8,6 +8,7 @@
 #include <valarray>
 #include <vector>
 
+
 namespace machine_learning{
     // 重载<< 打印二维数组
     template <typename T>
@@ -62,6 +63,108 @@ namespace machine_learning{
         }
         B[B.size() - 1] = ele;  // 最后位置插入新元素
         return B;
+    }
+
+    // 删除vector的是首个元素
+    template <typename T>
+    std::valarray<T> pop_front(const std::valarray<T> &A){
+        std::valarray <T> B;
+        B.resize(A.size() - 1);
+        for (size_t i = 1; i < A.size(); i++){
+            B[i - 1] = A[i];
+        }
+        return B;
+    }
+
+    // 移除vector的最后一个元素
+    template <typename T>
+    std::valarray<T> pop_back(const std::valarray<T> &A){
+        std::valarray<T> B;
+        B.resize(A.size() - 1);
+        for (size_t i = 0; i < A.size() - 1; i++){
+            B[i] = A[i];
+        }
+        return B;
+    }
+
+    // 打乱数据顺序 3D vector
+    template <typename T>
+    void  equal_shuffle(std::vector<std::vector<std::valarray<T>>> &A,
+                        std::vector<std::vector<std::valarray<T>>> &B){
+        // 确保A,B的size一致
+        if(A.size() != B.size()){
+            std::cerr << "ERROR (" << __func__ << ") : ";
+            std::cerr << "Can not equally shuffle two vector with difference sizes: ";
+            std::cerr << A.size() << " and " << B.size() << std::endl;
+            std::exit(EXIT_FAILURE);
+        }
+        for (size_t i = 0; i < A.size(); i++){
+            // 随机种子
+            std::srand(std::chrono::system_clock::now().time_since_epoch().count());
+            size_t random_index = std::rand() % A.size();
+            // 交换具有相同的随机index的元素
+            std::swap(A[i], A[random_index]);
+            std::swap(B[i], B[random_index]);
+        }
+        return;
+    }
+
+    // 使用uniform random initialization 初始化2D vector
+    template <typename T>
+    void uniform_random_initialization(std::vector<std::valarray<T>> &A,
+                                       const std::pair<size_t, size_t> &shape,
+                                       const T &low, const T &high){
+        // 确保A是空
+        A.clear();
+        // [low, high] 均匀分布
+        std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());
+        std::uniform_real_distribution<T> distibution(low, high);
+        for (size_t i = 0; i < shape.first; i++){
+            std::valarray<T> row; // 插入向量中的行
+            row.resize(shape.second); // 每行的元素个数，即列数
+            for(auto &r : row){
+                r = distibution(generator);  // 随机数
+            }
+            A.push_back(row);
+        }
+        return;
+    }
+
+    // 将2D vector 初始化为单位矩阵
+    template <typename T>
+    void unit_matrix_initialization(std::vector<std::valarray<T>> &A,
+                                    const std::pair<size_t, size_t> &shape){
+        A.clear();
+        for (size_t i = 0; i<shape.first; i++){
+            std::valarray<T> row;
+            row.resize(shape.second);
+            row[i] = T(1);  // 其它元素默认为0
+            A.push_back(row);
+        }
+        return;
+    }
+
+    // 将2D vector 初始化为0
+    template <typename T>
+    void zero_initialization(std::vector<std::valarray<T>> &A,
+                                    const std::pair<size_t, size_t> &shape){
+        A.clear();
+        for (size_t i = 0; i<shape.first; i++){
+            std::valarray<T> row;
+            row.resize(shape.second);  // 默认元素是0
+            A.push_back(row);
+        }
+        return;
+    }
+
+    // 计算2D vector中所有的元素之和
+    template <typename T>
+    T sum(const std::vector<std::valarray<T>> &A){
+        T cur_sum = 0;
+        for (const auto &a : A){
+            cur_sum += a.sum(); //a.sum()每一列的和
+        }
+        return cur_sum;
     }
 
     void test01(){
