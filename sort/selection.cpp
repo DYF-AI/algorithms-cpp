e43421b0e6b09edf1a9408edf0c99f0df7ee5ffd
DYF-AI <54470509+DYF-AI@users.noreply.github.com>
2021-03-15 23:19:06 +0800

    Add files via upload

---
+++ sort/selection.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+#include "common.cpp"
+
+
+template <typename T>
+void selcection(std::vector<T> &arr){
+    int N = arr.size();
+    for (int i = 0; i < N-1; i++){
+        int min = i;
+        for (int j = i + 1; j < N; j++){
+            if(arr[j] < arr[min]){
+                min = j;
+            }
+        }
+        std::swap(arr[i], arr[min]);
+    }
+}
+
+int main(int argc, char* argv[]){
+    std::vector<int> arr;
+    std::cout << argc << std::endl;
+    for (int i = 1; i < argc; i++){
+        arr.push_back(atoi(argv[i]));
+    }
+    print(arr);
+    selcection(arr);
+    print(arr);
+
+}
