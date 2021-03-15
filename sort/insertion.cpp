5fb5af993f59f8d4fe8215d7e0dfdd6fa49c696d
DYF-AI <54470509+DYF-AI@users.noreply.github.com>
2021-03-15 23:29:22 +0800

    Add files via upload

---
+++ sort/insertion.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+#include "common.cpp"
+
+template <typename T>
+void insertion(std::vector<T> &arr){
+    for (int i = 1; i < arr.size(); i++){
+        // 将arr[i] 插入到 arr[i-1] arr[i-2] arr[i-3]中
+        for (int j = i; j > 0 && (arr[j] < arr[j - 1]); j--){
+            std::swap(arr[j], arr[j - 1]);
+        }
+        print(arr);
+    }
+}
+
+int main(int argc, char* argv[]){
+    std::vector<int> arr;
+    std::cout << argc << std::endl;
+    for (int i = 1; i < argc; i++){
+        arr.push_back(atoi(argv[i]));
+    }
+    print(arr);
+    insertion(arr);
+    print(arr);
+}
